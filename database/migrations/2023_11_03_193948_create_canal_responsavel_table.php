dd79334ae81b37ea099de9ebc6f7f023c49cbf95
Luiz G. Nascimento <luiznascimento@carbonaut.io>
2023-11-15 22:41:42 -0300

    fix:(db): fix migration order

--- database/migrations/2023_11_03_193948_create_canal_responsavel_table.php
+++ database/migrations/2023_11_03_193948_create_canal_responsavel_table.php
@@ -4,21 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class() extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-
         Schema::create('canal_responsavel', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->foreignUuid('canal_id')->constrained('canais')->onDelete('cascade');
             $table->foreignUuid('responsavel_id')->constrained('responsaveis')->onDelete('cascade');
 
             $table->unique(['canal_id', 'responsavel_id']);
-            
+
             $table->timestamps();
         });
     }
